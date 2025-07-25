ec706b73fd3223deab878e614b2f31b3a55c9466
maguerrap <msguerra@uc.cl>
2025-07-25 15:13:30 -0500

    PEP8

--- vp_solver/src/jax_vp_solver.py
+++ vp_solver/src/jax_vp_solver.py
@@ -1,10 +1,11 @@
-import jax
-Array = jax.Array
-import jax.numpy as jnp
 import dataclasses
 from functools import partial
 from typing import Callable, Union
 
+import jax
+import jax.numpy as jnp
+
+Array = jax.Array
 
 
 @dataclasses.dataclass
@@ -19,14 +20,27 @@ class Mesh:
     period_v: float
     nx: int
     nv: int
-  
+
+
 def make_mesh(length_x: float, length_v: float, nx: int, nv: int) -> Mesh:
-    xs = jnp.linspace(0., length_x, nx, endpoint=False)
+    xs = jnp.linspace(0.0, length_x, nx, endpoint=False)
     dx = xs[1] - xs[0]
     vs = jnp.linspace(-length_v, length_v, nv, endpoint=False)
     dv = vs[1] - vs[0]
     V, X = jnp.meshgrid(vs, xs)
-    return Mesh(xs=xs, dx=dx, vs=vs, dv=dv, X=X, V=V, period_x=length_x, period_v=length_v, nx=nx, nv=nv)
+    return Mesh(
+        xs=xs,
+        dx=dx,
+        vs=vs,
+        dv=dv,
+        X=X,
+        V=V,
+        period_x=length_x,
+        period_v=length_v,
+        nx=nx,
+        nv=nv,
+    )
+
 
 @dataclasses.dataclass(frozen=True)
 class VlasovPoissonSolver:
@@ -34,15 +48,26 @@ class VlasovPoissonSolver:
     dt: float
     f_eq: Array
 
-
     def build_semilag_x(self) -> Callable[[Array], Array]:
         def interp_jax_x(f, v):
-            return jnp.interp(self.mesh.xs - 0.5 * v * self.dt, self.mesh.xs, f, period=self.mesh.period_x)
-        return jax.vmap(interp_jax_x, in_axes=(1,0), out_axes=1)
+            return jnp.interp(
+                self.mesh.xs - 0.5 * v * self.dt,
+                self.mesh.xs,
+                f,
+                period=self.mesh.period_x,
+            )
+
+        return jax.vmap(interp_jax_x, in_axes=(1, 0), out_axes=1)
 
     def build_semilag_v(self) -> Callable[[Array, Array], Array]:
         def interp_jax_v(f: Array, E: Array) -> Array:
-            return jnp.interp(self.mesh.vs - E * self.dt, self.mesh.vs, f, period=2 * self.mesh.period_v)
+            return jnp.interp(
+                self.mesh.vs - E * self.dt,
+                self.mesh.vs,
+                f,
+                period=2 * self.mesh.period_v,
+            )
+
         return jax.vmap(interp_jax_v, in_axes=(0, 0), out_axes=0)
 
     def compute_rho(self, f: Array) -> Array:
@@ -51,7 +76,17 @@ class VlasovPoissonSolver:
     def compute_E_from_rho(self, rho: Array) -> Array:
         rho_hat = jnp.fft.fft(rho)
         E_hat = jnp.zeros_like(rho_hat)
-        inv_multiplier = -1.0 / (1j * 2 * jnp.pi * jnp.fft.fftfreq(self.mesh.nx, d=self.mesh.period_x / self.mesh.nx)[1:])
+
+        inv_multiplier = -1.0 / (
+            1j
+            * 2
+            * jnp.pi
+            * jnp.fft.fftfreq(
+                self.mesh.nx,
+                d=self.mesh.period_x / self.mesh.nx,
+            )[1:]
+        )
+
         E_hat = E_hat.at[1:].set(inv_multiplier * rho_hat[1:])
         return jnp.real(jnp.fft.ifft(E_hat))
 
@@ -62,7 +97,9 @@ class VlasovPoissonSolver:
         return 0.5 * jnp.sum(jnp.square(E)) * self.mesh.dx
 
     @jax.jit
-    def time_step(self, f: Array, H: Array) -> tuple[Array, Array, Array]:
+    def time_step(
+        self, f: Array, H: Array
+    ) -> tuple[Array, Array, Array]:
         f_half = self.semilag_x(f)
         E = self.compute_E(f_half)
         E_total = E + H
@@ -70,7 +107,9 @@ class VlasovPoissonSolver:
         f = self.semilag_x(f)
         return f, f_half, E_total
 
-    def run_forward_jax_scan(self, f_iv: Array, H: Array, t_final: float) -> tuple:
+    def run_forward_jax_scan(
+        self, f_iv: Array, H: Array, t_final: float
+    ) -> tuple:
         num_steps = int(t_final / self.dt)
         f = f_iv.copy()
         tspan = self.dt * jnp.linspace(0, t_final, num_steps)
@@ -90,7 +129,8 @@ class VlasovPoissonSolver:
             f = semilag_x(f, self.mesh.vs)
             return f, (E_total, ee)
 
-        # Accumulate the final f, E, and electric energy ee
-        f_array, (E_array, ee_array) = jax.lax.scan(time_step_jax, f, tspan)
+        f_array, (E_array, ee_array) = jax.lax.scan(
+            time_step_jax, f, tspan
+        )
 
         return f_array, E_array, ee_array
--- vp_solver/src/utils.py
+++ vp_solver/src/utils.py
@@ -1,42 +1,44 @@
 import jax
 import jax.numpy as jnp
 
-import matplotlib.pyplot as plt
 import matplotlib
+import matplotlib.pyplot as plt
 
 matplotlib.rcParams.update({
-    'font.size': 14,  # General font size
-    'axes.labelsize': 14,  # Axis label size
-    'axes.titlesize': 14,  # Title size
-    'xtick.labelsize': 14,  # X-tick label size
-    'ytick.labelsize': 14,  # Y-tick label size
-    'legend.fontsize': 14   # Legend font size
+    'font.size': 14,
+    'axes.labelsize': 14,
+    'axes.titlesize': 14,
+    'xtick.labelsize': 14,
+    'ytick.labelsize': 14,
+    'legend.fontsize': 14
 })
 
 
+# ===== Auxiliary Functions =====
 
-
-
-### auxiliary_functions ###
-
-# Function to compute the external field H
 def external_electric_field(ak, mesh, k_0):
     N = ak.shape[1]
-    k = jnp.arange(1, N+1)
-    H = ak[0,:] @ jnp.cos(k_0 * k[:,None] * mesh.xs) + ak[1,:] @ jnp.sin(k_0 * k[:,None] * mesh.xs)
-    return H
+    k = jnp.arange(1, N + 1)
+    cos_term = ak[0, :] @ jnp.cos(k_0 * k[:, None] * mesh.xs)
+    sin_term = ak[1, :] @ jnp.sin(k_0 * k[:, None] * mesh.xs)
+    return cos_term + sin_term
 
 
-#### Cost functions #####
+# ===== Cost Functions =====
 
-#### KL ####
+# --- KL Divergence ---
 
 def kl_divergence(f_final, solver, eps=1e-12):
-    # Normalize the distributions so they sum to 1
-    f_final = f_final / (jnp.sum(f_final) * solver.mesh.dx * solver.mesh.dv + eps)
-    f_eq = solver.f_eq / (jnp.sum(solver.f_eq) * solver.mesh.dx * solver.mesh.dv + eps)
-    # Compute KL divergence, adding eps to avoid log(0)
-    kl_div = jnp.sum(jax.scipy.special.rel_entr(f_final, f_eq + eps) * solver.mesh.dx * solver.mesh.dv)
+    dx_dv = solver.mesh.dx * solver.mesh.dv
+    norm_final = jnp.sum(f_final) * dx_dv + eps
+    norm_eq = jnp.sum(solver.f_eq) * dx_dv + eps
+
+    f_final = f_final / norm_final
+    f_eq = solver.f_eq / norm_eq
+
+    kl_div = jnp.sum(
+        jax.scipy.special.rel_entr(f_final, f_eq + eps) * dx_dv
+    )
     return kl_div
 
 
@@ -51,23 +53,22 @@ def make_cost_function_kl(solver, solver_jit, f_iv, k_0, t_final):
     return cost_function_kl
 
 
-### EE ###
+# --- Final Electric Energy ---
 
 def make_cost_function_ee(solver, solver_jit, f_iv, k_0, t_final):
     @jax.jit
     def cost_function_ee(a_k):
         H = external_electric_field(a_k, solver.mesh, k_0)
         f_array, E_array, ee_array = solver_jit(f_iv, H, t_final)
-        cost = ee_array[-1]
-        return cost
+        return ee_array[-1]
 
     return cost_function_ee
 
 
-### EET ###
+# --- Total Electric Energy Over Time ---
 
 def electric_energy_in_time(ee_array, solver):
-    return jnp.sum(ee_array)*solver.dt
+    return jnp.sum(ee_array) * solver.dt
 
 
 def make_cost_function_eet(solver, solver_jit, f_iv, k_0, t_final):
@@ -75,18 +76,20 @@ def make_cost_function_eet(solver, solver_jit, f_iv, k_0, t_final):
     def cost_function_eet(a_k):
         H = external_electric_field(a_k, solver.mesh, k_0)
         f_array, E_array, ee_array = solver_jit(f_iv, H, t_final)
-        cost = electric_energy_in_time(ee_array, solver)
-        return cost
+        return electric_energy_in_time(ee_array, solver)
 
     return cost_function_eet
 
 
+# ===== Plotting Functions =====
 
-### Plotting functions ###
-# Function to plot the distribution of f_eq over x and v
 def plot_feq_distribution(fig, ax, f_eq, title, mesh, sci=False):
-    im = ax.imshow(f_eq.T, extent=[mesh.xs[0], mesh.xs[-1], mesh.vs[0], mesh.vs[-1]],
-                   aspect='auto', cmap='plasma')  # 'auto' aspect ratio
+    im = ax.imshow(
+        f_eq.T,
+        extent=[mesh.xs[0], mesh.xs[-1], mesh.vs[0], mesh.vs[-1]],
+        aspect='auto',
+        cmap='plasma'
+    )
     ax.set_title(title)
     ax.set_xlabel('$x$')
     ax.set_ylabel('$v$')
@@ -95,10 +98,13 @@ def plot_feq_distribution(fig, ax, f_eq, title, mesh, sci=False):
         cbar.ax.set_yscale('log')
 
 
-# Function to plot the distribution of f over x and v
 def plot_distribution(fig, ax, data, title, time, mesh, sci=False):
-    im = ax.imshow(data.T, extent=[mesh.xs[0], mesh.xs[-1], mesh.vs[0], mesh.vs[-1]],
-                   aspect='auto', cmap='plasma')  # Transpose data
+    im = ax.imshow(
+        data.T,
+        extent=[mesh.xs[0], mesh.xs[-1], mesh.vs[0], mesh.vs[-1]],
+        aspect='auto',
+        cmap='plasma'
+    )
     ax.set_title(f'{title} (T={time:.0f})')
     ax.set_xlabel('$x$')
     ax.set_ylabel('$v$')
@@ -107,17 +113,23 @@ def plot_distribution(fig, ax, data, title, time, mesh, sci=False):
         cbar.ax.set_yscale('log')
 
 
+def plot_inital_solve(
+    fig, axs, f_eq, f_array_1, ee_array_1, f_array_2, ee_array_2,
+    mesh, t_values, sci=False
+):
+    plot_feq_distribution(
+        fig, axs[0], f_eq, 'Distribution of $f_{eq}$', mesh, sci
+    )
+    plot_distribution(
+        fig, axs[1], f_array_1, 'Distribution of $f[H\\equiv 0]$',
+        t_values[-1], mesh, sci
+    )
+    plot_distribution(
+        fig, axs[2], f_array_2, 'Distribution of $f[H]$',
+        t_values[-1], mesh, sci
+    )
 
-
-
-def plot_inital_solve(fig, axs, f_eq, f_array_1, ee_array_1, f_array_2, ee_array_2, mesh, t_values, sci=False):
-    plot_feq_distribution(fig, axs[0], f_eq, 'Distribution of $f_{eq}$', mesh, sci)
-    
-    plot_distribution(fig, axs[1], f_array_1, 'Distribution of $f[H\\equiv 0]$', t_values[-1], mesh, sci)
-    
-    plot_distribution(fig, axs[2], f_array_2, 'Distribution of $f[H]$', t_values[-1], mesh, sci)
-
-    axs[3].ticklabel_format(axis='y', style='sci', scilimits=(-2,2))
+    axs[3].ticklabel_format(axis='y', style='sci', scilimits=(-2, 2))
     axs[3].plot(t_values, ee_array_1, label="No $H$")
     axs[3].plot(t_values, ee_array_2, label="Good initial $H$")
     axs[3].set_xlabel('$t$')
@@ -125,65 +137,65 @@ def plot_inital_solve(fig, axs, f_eq, f_array_1, ee_array_1, f_array_2, ee_array
     axs[3].legend()
 
 
-def plot_results_TS(fig, axs, f_final, E_array, H, ee_array, objective_values, t_values, mesh):
+def plot_results_TS(
+    fig, axs, f_final, E_array, H, ee_array, objective_values,
+    t_values, mesh
+):
     dt = t_values[1] - t_values[0]
-    
-    plot_distribution(fig, axs[0], f_final, 'Distribution of $f[H]$', t_values[-1], mesh)
-    
 
-    axs[1].ticklabel_format(axis='y', style='sci', scilimits=(-2,2))
+    plot_distribution(
+        fig, axs[0], f_final, 'Distribution of $f[H]$',
+        t_values[-1], mesh
+    )
+
+    axs[1].ticklabel_format(axis='y', style='sci', scilimits=(-2, 2))
     axs[1].plot(mesh.xs, H, label='$H(x)$')
-    axs[1].plot(mesh.xs, E_array[0] - H, label=f'$E(t={0*dt:.0f},x)$')
-    axs[1].plot(mesh.xs, E_array[99] - H, label=f'$E(t={100*dt:.0f},x)$')
-    axs[1].plot(mesh.xs, E_array[199] - H, label=f'$E(t={200*dt:.0f},x)$')
-    axs[1].plot(mesh.xs, E_array[299] - H, label=f'$E(t={300*dt:.0f},x)$')
+    axs[1].plot(mesh.xs, E_array[0] - H, label='$E(t=0,x)$')
+    axs[1].plot(mesh.xs, E_array[99] - H, label='$E(t=100,x)$')
+    axs[1].plot(mesh.xs, E_array[199] - H, label='$E(t=200,x)$')
+    axs[1].plot(mesh.xs, E_array[299] - H, label='$E(t=300,x)$')
     axs[1].set_xlabel('$x$')
     axs[1].set_title('Electric fields')
     axs[1].legend(loc='upper right')
-    
-    
-    
-    axs[2].ticklabel_format(axis='y', style='sci', scilimits=(-2,2))
+
+    axs[2].ticklabel_format(axis='y', style='sci', scilimits=(-2, 2))
     axs[2].plot(t_values, ee_array)
     axs[2].set_xlabel('$t$')
     axs[2].set_title('$\\mathcal{E}_{f}(t)$')
-    
-    
-    #axs[3].ticklabel_format(axis='both', style='sci', scilimits=(0,0))#, useMathText=True)
-    axs[3].plot(objective_values)#, label='L-BFGS')
+
+    axs[3].plot(objective_values)
     axs[3].set_yscale('log')
     axs[3].set_xlabel("Iteration")
     axs[3].set_title("Convergence of Objective")
 
 
-
-def plot_results_BoT(fig, axs, f_final, E_array, H, ee_array, objective_values, t_values, mesh):
+def plot_results_BoT(
+    fig, axs, f_final, E_array, H, ee_array, objective_values,
+    t_values, mesh
+):
     dt = t_values[1] - t_values[0]
-    
-    plot_distribution(fig, axs[0], f_final, 'Distribution of $f[H]$', t_values[-1], mesh)
-    
 
-    axs[1].ticklabel_format(axis='y', style='sci', scilimits=(-2,2))
+    plot_distribution(
+        fig, axs[0], f_final, 'Distribution of $f[H]$',
+        t_values[-1], mesh
+    )
+
+    axs[1].ticklabel_format(axis='y', style='sci', scilimits=(-2, 2))
     axs[1].plot(mesh.xs, H, label='$H(x)$')
-    axs[1].plot(mesh.xs, E_array[0] - H, label=f'$E(t={0*dt:.0f},x)$')
-    axs[1].plot(mesh.xs, E_array[199] - H, label=f'$E(t={200*dt:.0f},x)$')
-    axs[1].plot(mesh.xs, E_array[299] - H, label=f'$E(t={300*dt:.0f},x)$')
-    axs[1].plot(mesh.xs, E_array[399] - H, label=f'$E(t={400*dt:.0f},x)$')
+    axs[1].plot(mesh.xs, E_array[0] - H, label='$E(t=0,x)$')
+    axs[1].plot(mesh.xs, E_array[199] - H, label='$E(t=200,x)$')
+    axs[1].plot(mesh.xs, E_array[299] - H, label='$E(t=300,x)$')
+    axs[1].plot(mesh.xs, E_array[399] - H, label='$E(t=400,x)$')
     axs[1].set_xlabel('$x$')
     axs[1].set_title('Electric fields')
     axs[1].legend(loc='upper right')
-    
-    
-    
-    axs[2].ticklabel_format(axis='y', style='sci', scilimits=(-2,2))
+
+    axs[2].ticklabel_format(axis='y', style='sci', scilimits=(-2, 2))
     axs[2].plot(t_values, ee_array)
     axs[2].set_xlabel('$t$')
     axs[2].set_title('$\\mathcal{E}_{f}(t)$')
-    
-    
-    #axs[3].ticklabel_format(axis='both', style='sci', scilimits=(0,0))#, useMathText=True)
-    axs[3].plot(objective_values)#, label='L-BFGS')
+
+    axs[3].plot(objective_values)
     axs[3].set_yscale('log')
     axs[3].set_xlabel("Iteration")
     axs[3].set_title("Convergence of Objective")
-
