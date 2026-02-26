c0330b3be3fd3db8539d3b93e454687118af90ce
maguerrap <msguerra@uc.cl>
2026-02-26 12:33:42 -0600

    updated tests

--- tests/jax_vp_test.py
+++ tests/jax_vp_test.py
@@ -155,7 +155,7 @@ def test_mass_conservation(solver, f0):
     massT = mass(f_array)
 
     assert jnp.isclose(
-        mass0, massT, rtol=1e-6, atol=1e-8
+        mass0, massT, rtol=1e-4, atol=1e-6
     )
 
 
